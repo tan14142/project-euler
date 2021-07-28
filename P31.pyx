e108070cbfec295fea28c26fee73221601cd23f5
Tuan Anh Nguyen <tan14142@gmail.com>
2021-07-28 23:25:48 +1200

    free

--- P31.pyx
+++ P31.pyx
@@ -1,25 +1,21 @@
 from libc.stdlib cimport calloc, malloc, free
 
 def solve(int target):
-  cdef int coins[8], coin, i, k, ln
+  cdef int coins[8], coin, i
+  cdef int* mem = <int*> calloc(target + 1, (target + 1) * sizeof(int))
 
   coins = [1, 2, 5, 10, 20, 50, 100, 200]
-  ln = len(coins)
 
-  cdef int** mem = <int**> malloc(ln * sizeof(int*))
+  if not mem:
+    raise MemoryError()
 
-  for i in range(ln):
-    mem[i] = <int*> malloc((target + 1) * sizeof(int))
-  
-  for k, coin in enumerate(coins):
-    for i in range(1, target + 1):
-      mem[k][i] = i // coin
+  mem[0] = 1
 
-  cdef int* result = <int*> calloc(target + 1, (target + 1) * sizeof(int))
-  result[0] = 1
+  try:
+    for coin in coins:
+      for i in range(coin, target + 1):
+        mem[i] += mem[i - coin]
 
-  for k, coin in enumerate(coins):
-    for i in range(coin, target + 1):
-      result[i] += result[i - coin]
-  
-  return result[target]
+    return mem[target]
+  finally:
+    free(mem)
